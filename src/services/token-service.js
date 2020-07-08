f0bb52324119e3ad89266cbe35d459efdf86be14
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-08 18:16:47 -0500

    issue with upload - jwt token

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -2,15 +2,15 @@ import config from "../config";
 
 const TokenService = {
 	saveAuthToken(token) {
-		window.sessionStorage.setItem(config.TOKEN_KEY, token);
+		window.localStorage.setItem(config.TOKEN_KEY, token);
 	},
 
 	getAuthToken() {
-		return window.sessionStorage.getItem(config.TOKEN_KEY);
+		return window.localStorage.getItem(config.TOKEN_KEY);
 	},
 
 	clearAuthToken() {
-		return window.sessionStorage.removeItem(config.TOKEN_KEY);
+		return window.localStorage.removeItem(config.TOKEN_KEY);
 	},
 
 	hasAuthToken() {
