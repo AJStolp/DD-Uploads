aa791d9b4a0f81ca47cb6e7291f622f18a3480bc
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-10 14:42:11 -0500

    gcs no cors

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -5,7 +5,7 @@ const AuthApiService = {
 		return fetch(`${config.API_ENDPOINT}/auth/login`, {
 			method: "POST",
 			headers: {
-				"content-type": "application/json",
+				// "content-type": "application/json",
 			},
 			body: JSON.stringify({ username, password }),
 		}).then((res) =>
@@ -16,7 +16,7 @@ const AuthApiService = {
 		return fetch(`${config.API_ENDPOINT}/users`, {
 			method: "POST",
 			headers: {
-				"content-type": "application/json",
+				// "content-type": "application/json",
 			},
 			body: JSON.stringify(user),
 		}).then((res) =>
