da7d6d67971539b216e3b9e6604fbb04c8465924
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-15 15:32:42 -0500

    wave error fix

--- src/upload-videos/upload-videos.js
+++ src/upload-videos/upload-videos.js
@@ -48,7 +48,7 @@ class UploadVideos extends React.Component {
 		const url = `${config.API_ENDPOINT}/videos`;
 
 		trackPromise(
-			fetch(url, request)
+			fetch(url, {mode: "no-cors"}, request)
 				.then((res) => {
 					if (!res.ok) {
 						throw new Error(
