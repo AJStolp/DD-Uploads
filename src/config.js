782c787b0e869133f71d9870bb423f5d632eafcf
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-16 12:12:51 -0500

    gcs issue, heroku

--- src/config.js
+++ src/config.js
@@ -2,3 +2,5 @@ export default {
 	API_ENDPOINT: "https://shrouded-inlet-41818.herokuapp.com/api",
 	TOKEN_KEY: "dduploads-client-auth-token",
 };
+// "https://shrouded-inlet-41818.herokuapp.com/api"
+// "http://localhost:8000/api"
