3c4a667613b3cb964f7f7e02175bf590f5ecda65
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-22 16:21:44 -0500

    new link

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
 	// API_ENDPOINT: "http://localhost:8000/api",
-	API_ENDPOINT: "https://lit-mountain-80792.herokuapp.com/",
+	API_ENDPOINT: "https://lit-mountain-80792.herokuapp.com/api",
 	TOKEN_KEY: "dduploads-client-auth-token",
 };
 // "https://lit-mountain-80792.herokuapp.com/"
