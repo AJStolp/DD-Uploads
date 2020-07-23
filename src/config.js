480476e27bb52243e324cebc92c81f15d254ea57
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-23 15:10:28 -0500

    upload working on production

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
 export default {
-	// API_ENDPOINT: "http://localhost:8000/api",
 	API_ENDPOINT: "https://lit-mountain-80792.herokuapp.com/api",
 	TOKEN_KEY: "dduploads-client-auth-token",
 };
-// "https://lit-mountain-80792.herokuapp.com/"
-// "http://localhost:8000/api"
